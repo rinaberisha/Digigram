ae3d3e5529ea844669296a29662b2c20dfd1a92f
rinaberisha <rinaberisha96@hotmail.com>
2022-02-02 12:34:37 +0100

    Completed initial files

---
+++ main.js
@@ -0,0 +1,38 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+///////
+  $('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    
+    {
+      duration: 2500,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+    });  
+  });
